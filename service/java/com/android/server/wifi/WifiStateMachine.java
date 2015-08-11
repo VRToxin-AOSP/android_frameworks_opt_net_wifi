bfd5a550dcf0c04d10e5bb7e8dcacb0b7f391605
Matthieu MAUGER <matthieux.mauger@intel.com>
2015-08-11 10:04:47 +0200

    Limit WiFi scans while P2P is connected
    This patch aims to discard all periodical and Google apps trigged
    WiFi scans while P2P is connecting / connected.
    
    Only scans comming from user applications (like Settings) are
    allowed.
    
    This leads to a stable and performant P2P link suitable for WiDi
    usage.
    
    Change-Id: Ie65c833c11d5ba5e6753eb864a40e0633d973099
    Signed-off-by: Matthieu MAUGER <matthieux.mauger@intel.com>
    Signed-off-by: Aymen Ben Alaya <aymenx.ben.alaya@intel.com>
    Signed-off-by: Zhiquan Liu <zhiquan.liu@intel.com>

--- service/java/com/android/server/wifi/WifiStateMachine.java
+++ service/java/com/android/server/wifi/WifiStateMachine.java
@@ -198,6 +198,7 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
     private WifiApConfigStore mWifiApConfigStore;
     private final boolean mP2pSupported;
     private final AtomicBoolean mP2pConnected = new AtomicBoolean(false);
+    private final AtomicBoolean mP2pConnecting = new AtomicBoolean(false);
     private boolean mTemporarilyDisconnectWifi = false;
     private final String mPrimaryDeviceType;
 
@@ -1960,9 +1961,43 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
         }
     }
 
+    private boolean discardScanWhenP2pConnected() {
+
+        if (!mContext.getResources().getBoolean(
+                 R.bool.config_discard_scan_when_p2p_connected)) {
+            if (VDBG) logi("config_discard_scan_when_p2p_connected is not set");
+            return false;
+        }
+
+        if (mP2pConnected.get() || mP2pConnecting.get()) {
+            if (VDBG) logi("mP2pConnected = " + mP2pConnected.get()
+                    + " mP2pConnecting = " + mP2pConnecting.get());
+            return true;
+        }
+
+        return false;
+    }
+
     private void handleScanRequest(int type, Message message) {
         ScanSettings settings = null;
         WorkSource workSource = null;
+        int callingUid = message.arg1;
+
+        // check if scan is allowed
+        if (discardScanWhenP2pConnected()) {
+            if (callingUid == SCAN_ALARM_SOURCE) {
+                return;
+            }
+            try {
+                int googleAppsUid = mContext.getPackageManager().getPackageInfo(
+                        "com.google.android.gms", 0).applicationInfo.uid;
+                if (callingUid == googleAppsUid) {
+                    return;
+                }
+            } catch (PackageManager.NameNotFoundException e) {
+                loge("Unable to find com.google.android.gms package");
+            }
+        }
 
         // unbundle parameters
         Bundle bundle = (Bundle) message.obj;
@@ -5719,6 +5754,7 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
                 case WifiP2pServiceImpl.P2P_CONNECTION_CHANGED:
                     NetworkInfo info = (NetworkInfo) message.obj;
                     mP2pConnected.set(info.isConnected());
+                    mP2pConnecting.set(info.getState() == NetworkInfo.State.CONNECTING);
                     break;
                 case WifiP2pServiceImpl.DISCONNECT_WIFI_REQUEST:
                     mTemporarilyDisconnectWifi = (message.arg1 == 1);
@@ -9554,6 +9590,7 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
                 case WifiP2pServiceImpl.P2P_CONNECTION_CHANGED:
                     NetworkInfo info = (NetworkInfo) message.obj;
                     mP2pConnected.set(info.isConnected());
+                    mP2pConnecting.set(info.getState() == NetworkInfo.State.CONNECTING);
                     if (mP2pConnected.get()) {
                         mWifiNative.setScanInterval((int)mScanIntervalP2pConnectedMs/1000);
                     } else {
