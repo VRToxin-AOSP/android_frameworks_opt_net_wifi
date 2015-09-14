5caaf9ec730007558597bc031c67cec8139a2d94
Om Prakash Singh <omp@nvidia.com>
2015-09-14 14:39:06 +0530

    fix intent package
    currently intent package is being set as com.android.server.wifi but
    the intent received is registered with context that system server.
    
    Change-Id: I4767f4624a939e49a26656185e8155546a187923

--- service/java/com/android/server/wifi/WifiStateMachine.java
+++ service/java/com/android/server/wifi/WifiStateMachine.java
@@ -1362,8 +1362,7 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
     PendingIntent getPrivateBroadcast(String action, int requestCode) {
         Intent intent = new Intent(action, null);
         intent.addFlags(Intent.FLAG_RECEIVER_REGISTERED_ONLY_BEFORE_BOOT);
-        // TODO: Find the correct value so this is not hard coded
-        intent.setPackage("android");
+        intent.setPackage(mContext.getPackageName());
         return PendingIntent.getBroadcast(mContext, requestCode, intent, 0);
     }
 
@@ -6450,7 +6449,7 @@ public class WifiStateMachine extends StateMachine implements WifiNative.WifiPno
 
                     /* send regular delayed shut down */
                     Intent driverStopIntent = new Intent(ACTION_DELAYED_DRIVER_STOP, null);
-                    driverStopIntent.setPackage("android");
+                    driverStopIntent.setPackage(mContext.getPackageName());
                     driverStopIntent.putExtra(DELAYED_STOP_COUNTER, mDelayedStopCounter);
                     mDriverStopIntent = PendingIntent.getBroadcast(mContext,
                             DRIVER_STOP_REQUEST, driverStopIntent,
